aaecd44b3a3bbb744f6e82759705afc2fddae152
Joseph Bonneau <jbonneau@gmail.com>
2014-02-05 12:51:02 -0800

    fixed missing quote

--- data/fetch.sh
+++ data/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl 'http://blockchain.info/charts/hash-rate?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=json&address=' -o 'hash_rate.json'
-curl 'http://blockchain.info/charts/market-price?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=json&address=' -o price.json
+curl 'http://blockchain.info/charts/market-price?showDataPoints=false&timespan=all&show_header=true&daysAverageString=1&scale=0&format=json&address=' -o 'price.json'
 
