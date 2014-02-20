fa706cd72be35020a0ed6d52e46d321e35719746
Joseph Bonneau <jbonneau@gmail.com>
2014-02-19 23:32:19 -0500

    choose custom date ranges

--- aggregate.py
+++ aggregate.py
@@ -1,46 +1,71 @@
 #!/usr/bin/env python
-from datetime import date
+from datetime import date, timedelta
 import math
 
 # local files
 from constants import *
 
-total_work = 0
-total_reward = 0
+def compute_total_work(start_date, end_date):
+  total_work = 0
+  for i in xrange(len(capacities) - 1):
+    t1, r1 = capacities[i]
+    t2, r2 = capacities[i + 1]
 
-start_date = genesis_date
-end_date = date.today()
+    t1 = max(start_date, t1)
+    t2 = min(end_date, t2)
+    if t1 > t2: continue
 
-for i in xrange(len(capacities) - 1):
-  t1, r1 = capacities[i]
-  t2, r2 = capacities[i + 1]
+    t = (t2 - t1).total_seconds()
+    r = (r1 + r2) / 2 #trapezoidal interpolation
 
-  t = (t2 - t1).total_seconds()
-  r = (r1 + r2) / 2 #trapezoidal interpolation
+    total_work += t * r
 
-  total_work += t * r
+  return total_work
 
-reward_index = 0
-for i in xrange(len(usd_prices) - 1):
-  t1, r1 = usd_prices[i]
-  t2, r2 = usd_prices[i + 1]
+def compute_total_reward(start_date, end_date):
+  total_reward = 0
+  reward_index = 0
+  for i in xrange(len(usd_prices) - 1):
+    t1, r1 = usd_prices[i]
+    t2, r2 = usd_prices[i + 1]
 
-  reward = block_rewards[reward_index][1]
-  if t2 > block_rewards[reward_index + 1][0]:
-    reward_index += 1
-    d1 = (block_rewards[reward_index][0] - t1).total_seconds()
-    d2 = (t2 - block_rewards[reward_index][0]).total_seconds()
-    reward = (block_rewards[reward_index - 1][1] * d1)/(d1 + d2) + (block_rewards[reward_index ][1] * d2)/(d1 + d2)
+    t1 = max(start_date, t1)
+    t2 = min(end_date, t2)
+    if t1 > t2: continue
 
-  t = (t2 - t1).total_seconds() / (60 * 10)
-  r = (r1 + r2) / 2 #trapezoidal interpolation
+    while t1 > block_rewards[reward_index + 1][0]:
+      reward_index += 1
+
+    reward = block_rewards[reward_index][1]
+    if t2 > block_rewards[reward_index + 1][0]:
+      reward_index += 1
+      d1 = (block_rewards[reward_index][0] - t1).total_seconds()
+      d2 = (t2 - block_rewards[reward_index][0]).total_seconds()
+      reward = (block_rewards[reward_index - 1][1] * d1)/(d1 + d2) + (block_rewards[reward_index ][1] * d2)/(d1 + d2)
+
+    t = (t2 - t1).total_seconds() / (60 * 10) #block frequency
+    r = (r1 + r2) / 2 #trapezoidal interpolation
+    
+    total_reward += r * t * reward
+
+  return total_reward
+
+
+date_ranges = [(genesis_date, date.today())]
+
+extended_ranges = []
+d = date(2013, 01, 01)
+while d < date.today():
+  extended_ranges.append((d, date.today()))
+  d += timedelta(days = 30)
   
- 
-  total_reward += r * t * reward
+#date_ranges += extended_ranges
 
-start_date = max(start_date, genesis_date)
-end_date = min(end_date, capacities[-1][0])
+for (s, e) in date_ranges:
 
-print "Aggregate Bitcoin network from %s to %s:" % (start_date, end_date)
-print "\t2^%2f hashes computed" % (math.log(total_work, 2))
-print "\t%.1f M US$ earned" % (total_reward / MH)
+  s = max(s, genesis_date)
+  e = min(e, capacities[-1][0])
+
+  print "Aggregate Bitcoin network from %s to %s:" % (s, e)
+  print "\t2^%.3f hashes computed" % (math.log(compute_total_work(s, e), 2))
+  print "\t%.1f M US$ earned" % (compute_total_reward(s, e) / MH)
